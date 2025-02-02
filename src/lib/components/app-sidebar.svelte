87a40c85d03920745161b42024269212f52b80bf
lev <aw@a0.ax>
2025-02-01 22:29:15 -0600

    Add dark mode toggle

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import * as Sidebar from "$lib/components/ui/sidebar/index.js";
     import type { ComponentProps } from "svelte";
-    import { Button } from '$lib/components/ui/button';
-    import { House, Dumbbell, Bike, ChartColumnBig, ChartLine, Calendar, ChartPie, Library, Map } from 'lucide-svelte';
+    import { House, Dumbbell, Bike, ChartColumnBig, ChartLine, Calendar, ChartPie, Library, Map, Sun, Moon } from 'lucide-svelte';	
+	import { toggleMode } from "mode-watcher";
+	import { Button } from "$lib/components/ui/button/index.js";
 
     let { ref = $bindable(null), ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
 </script>
@@ -15,6 +16,17 @@
                 <p>v0.1</p>
                 <p>by <a href="https://a0.ax">lev</a></p>
             </div>
+			<div class="h-full pr-1">
+				<Button variant="outline" size="icon" onclick={toggleMode}>
+					<Sun
+					class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+					/>
+					<Moon
+					class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+					/>
+					<span class="sr-only">Toggle theme</span>
+				</Button>
+			</div>
         </div>
     </Sidebar.Header>
     <Sidebar.Content>
