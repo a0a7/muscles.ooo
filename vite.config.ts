722d88729dbbcbfd95042c06c7e7a4cccd2510b9
aw <aw@a0.ax>
2025-03-20 14:45:15 -0500

    remove thing from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  build: {
-    rollupOptions: {
-      external: ['d3-format']
-    }
-  }
+  plugins: [sveltekit()]
 });
